614582d09b70681f7858ac9375c21913abf581a9
shingoed <nakajima.shingo@gmail.com>
2019-10-29 14:45:57 -0700

    finished

---
+++ app.js
@@ -0,0 +1,69 @@
+'use strict';
+
+var pencil = prompt('Is there a pencil type named Ticonderoga No. 2 pencil?');
+// always have the lowercase function before it processes the reply from user
+pencil = pencil.toLowerCase()
+if (pencil === 'yes') {
+    // console.log('You are correct!')
+}
+else if (pencil === 'no') {
+    // console.log('You are wrong!')
+    pencil = pencil.toLowerCase()
+}
+
+var pencil = alert('The answer is yes')
+
+
+var einstein = prompt('Albert Einstein failed every subject in school that wasnt math or physics');
+einstein = einstein.toLowerCase()
+if (einstein === 'yes') {
+    // console.log('You are correct!')
+}
+else if (einstein === 'no') {
+    // console.log('You are wrong!')
+    einstein = einstein.toLowerCase()
+}
+
+var einstein = alert('The answer is yes')
+
+var nascar = prompt('Do NASCAR engines burn 115-octane leaded gasoline?');
+nascar = nascar.toLowerCase()
+if (nascar === 'yes') {
+    // console.log('You are correct!')
+}
+else if (nascar === 'no') {
+    // console.log('You are wrong!')
+    nascar = nascar.toLowerCase()
+}
+
+var nascar = alert('The answer is no')
+
+var natural = prompt('Is Sucralose natural?');
+natural = natural.toLowerCase()
+if (natural === 'yes') {
+    // console.log('You are correct!')
+}
+else if (natural === 'no') {
+    // console.log('You are wrong!')
+    natural = natural.toLowerCase()
+}
+
+var natural = alert('The answer is no')
+
+var horseradish = prompt('Is Horseradish sauce of German origin?');
+horseradish = horseradish.toLowerCase()
+if (horseradish === 'yes') {
+    // console.log('You are correct!')
+}
+else if (horseradish === 'no') {
+    // console.log('You are wrong!')
+    horseradish = horseradish.toLowerCase()
+}
+
+var horseradish = alert('The answer is yes')
+
+
+var name = prompt('What is your name?');
+var display = alert('Welcome' + name + '!!');
+
+
